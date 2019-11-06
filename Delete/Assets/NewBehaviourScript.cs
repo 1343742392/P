6baf29f7cbf2d74bf87a618833f0ff9a869417e1
1343742392 <35752113+1343742392@users.noreply.github.com>
2019-11-06 09:36:17 +0800

    1

---
+++ Delete/Assets/NewBehaviourScript.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public const int ADD = 1;
+    public const int LOSS = 2;
+
+
+    static public float CycleLimit(float source, float obj, float min, float max, int operation)
+    {
+        if (source < min || source > max)
+        {
+            Debug.Log("非正常输入");
+            return 0;
+        }
+        switch (operation)
+        {
+            case ADD:
+                if (source + obj > max)
+                {
+                    return min + (source + obj) - max;
+                }
+                else if (source + obj < min)
+                {
+                    return max - min + (source + obj);
+                }
+                return source + obj;
+            case LOSS:
+                break;
+        }
+        return 0;
+    }
+
+    static public Vector3 Rotate(Vector3 rotation, Vector3 source, Vector3 axis, float angle)
+    {
+        float axisX = Mathf.Abs(axis.x - (90 - Mathf.Abs(rotation.x % 180 - 90)) / 90);
+        float axisY = Mathf.Abs(axis.y - (90 - Mathf.Abs(rotation.y % 180 - 90)) / 90);
+        float axisZ = Mathf.Abs(axis.z - (90 - Mathf.Abs(rotation.z % 180 - 90)) / 90);
+        Debug.Log(axisX + "  " + axisY + "  " + axisZ);
+        Quaternion q = Quaternion.AngleAxis(angle, new Vector3(axisX, axisY, axisZ));// 旋转系数
+        return q * source;
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+
+
+    }
+
+
+    static public Vector3 Rotate(Vector3 source, Vector3 axis, float angle)
+    {
+
+        Quaternion q = Quaternion.AngleAxis(angle, axis);// 旋转系数
+        return q * source;
+    }
+
+    float getAxis(float axis, float rotation)
+    {
+
+    }
+  
+    void Update()
+    {
+        Vector3 inputAxis = new Vector3(1, 0, 0);
+
+        Vector3 axis;
+        float angle;
+        transform.rotation.ToAngleAxis(out angle, out axis);
+        Vector3 rotation = angle * axis;
+        float axisX = (90 - Mathf.Abs(rotation.x % 180 - 90)) / 90;
+        float axisY = (90 - Mathf.Abs(rotation.y % 180 - 90)) / 90;
+        float axisZ = (90 - Mathf.Abs(rotation.z % 180 - 90)) / 90;
+
+        Debug.Log()
+/*
+        Debug.Log("axis;" + axisX + ";" + axisY + ";" + axisZ + ";" + "input" + rotation + "out" + 
+            new Vector3(
+                getAxis(inputAxis.x, axisZ),
+                getAxis(inputAxis.y, (axisX + axisZ) / 2),
+                getAxis(inputAxis.z, (axisX + axisY) / 2)
+                ));
+                */
+
+    }
+}
---
+++ Delete/Assets/shere.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shere : MonoBehaviour {
+    [SerializeField] bool old;
+    static public Vector3 Rotate(Vector3 source, Vector3 axis, float angle)
+    {
+
+        Quaternion q = Quaternion.AngleAxis(angle, axis);// 旋转系数
+        return q * source;
+    }
+
+    Vector3 RoteX(Vector3 value, float angle)
+    {
+        Matrix4x4 result = new Matrix4x4();
+        result.SetRow(0, new Vector4(1f, 0f, 0f, 0f));
+        result.SetRow(1, new Vector4(0f, Mathf.Cos(angle), -Mathf.Sin(angle), 0f));
+        result.SetRow(2, new Vector4(0f, Mathf.Sin(angle), Mathf.Cos(angle), 0f));
+        result.SetRow(3, new Vector4(0f, 0f, 0f, 1f));
+
+        return result.MultiplyVector(new Vector4(value.x, value.y, value.z, 1));
+    }
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(RoteX(new Vector3(0.2f, 0.5f, 1), 3.14f/2));
+        Debug.Log(Rotate(new Vector3(0.2f, 0.5f, 1),new Vector3(1, 0, 0), 90));
+        if(old)
+            transform.position = GameObject.Find("Cube").transform.position + Rotate(GameObject.Find("Cube").transform.forward,new Vector3(1,0,0), 90) * 2;
+        else
+            transform.position = GameObject.Find("Cube").transform.position + RoteX(GameObject.Find("Cube").transform.forward, 3.14f / 2) * 2;
+    }
+}
